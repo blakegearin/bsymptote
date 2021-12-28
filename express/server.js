0cd5f50c7cf5daaa5eb6e13053f748ee8f831b4c
Blake Gearin <blakegearin@pm.me>
2021-12-28 17:15:48 -0600

    Revert server.js to working example

--- express/server.js
+++ express/server.js
@@ -1,46 +1,50 @@
 'use strict';
 
-// const express = require('express');
-// const path = require('path');
-// const serverless = require('serverless-http');
-// const app = express();
-// const bodyParser = require('body-parser');
-
-// const router = express.Router();
-// router.get('/', (req, res) => {
-//   res.writeHead(200, { 'Content-Type': 'text/html' });
-//   res.write('<h1>Hello from Express.js!</h1>');
-//   res.end();
-// });
-// router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-// router.post('/', (req, res) => res.json({ postBody: req.body }));
-
-// app.use(bodyParser.json());
-// app.use('/.netlify/functions/server', router);  // path must route to lambda
-// app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
-
-
-
-
-
 const express = require('express');
-const app = express();
 const path = require('path');
+const serverless = require('serverless-http');
+const app = express();
+const bodyParser = require('body-parser');
+
 const router = express.Router();
-
-// app.listen(3000, () => {
-//   console.log("Application started and listening on port 3000");
-// });
-
-app.use('/static', express.static(path.join(__dirname, 'public')));
-app.use('/scripts', express.static(path.join(__dirname, 'node_modules/paper/dist')));
-
 router.get('/', (req, res) => {
-  res.sendFile(__dirname + "../views/genuary-1-2022.html");
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
 });
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
 
+app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
+
+
+
+
+// 'use strict';
+
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+// const router = express.Router();
+
+// // app.listen(3000, () => {
+// //   console.log("Application started and listening on port 3000");
+// // });
+
+// app.use('/static', express.static(path.join(__dirname, 'public')));
+// app.use('/scripts', express.static(path.join(__dirname, 'node_modules/paper/dist')));
+
+// router.get('/', (req, res) => {
+//   res.sendFile(__dirname + "../views/genuary-1-2022.html");
+// });
+
+// app.use('/.netlify/functions/server', router);  // path must route to lambda
+// app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+// module.exports = app;
+// module.exports.handler = serverless(app);

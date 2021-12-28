5b79e10239415e387ff20463f8742d8dc6762dfa
Blake Gearin <blakegearin@pm.me>
2021-12-28 17:38:08 -0600

    Updated get / logic

--- express/server.js
+++ express/server.js
@@ -11,12 +11,14 @@ const router = express.Router();
 app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use('/scripts', express.static(path.join(__dirname, 'node_modules/paper/dist')));
 
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-  // res.sendFile(__dirname + "../views/genuary-1-2022.html");
-});
+// router.get('/', (req, res) => {
+//   // res.writeHead(200, { 'Content-Type': 'text/html' });
+//   // res.write('<h1>Hello from Express.js!</h1>');
+//   // res.sendFile(__dirname + "../views/genuary-1-2022.html");
+//   res.sendFile(path.join(__dirname, "../views/genuary-1-2022.html");
+//   res.end();
+// });
+router.get('/', (req, res) => res.sendFile(path.join(__dirname, "../views/genuary-1-2022.html")));
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 

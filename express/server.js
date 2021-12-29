2e6b817484544e3fe2d65b7f4acd8c1788bad63f
Blake Gearin <blakegearin@pm.me>
2021-12-29 00:21:26 -0600

    Working locally

--- express/server.js
+++ express/server.js
@@ -8,21 +8,43 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 
-app.use('/static', express.static(path.join(__dirname, 'public')));
-app.use('/scripts', express.static(path.join(__dirname, 'node_modules/paper/dist')));
+// router.use('/static', express.static(path.join(__dirname, 'public')));
+// router.use('/scripts', express.static(path.join(__dirname, 'node_modules/paper/dist')));
 
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<!DOCTYPE html><html><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="stylesheet" href="/static/stylesheets/style.css" as="style"><script type="text/javascript" src="/scripts/paper-full.js"></script><script type="text/javascript" src="/static/javascripts/genuary-1-2022.js" canvas="myCanvas"></script></head><body><canvas id="myCanvas"></canvas></body></html>');
-  // res.sendFile(__dirname + "../views/genuary-1-2022.html");
-  // res.sendFile(path.join(__dirname, "../views/genuary-1-2022.html"));
-  // res.sendFile(path.join(__dirname, "../views/genuary-1-2022.html");
-  res.end();
-});
+app.use(express.static('public'));
+app.use('/scripts', express.static('node_modules/paper/dist'));
+
+// router.get('/', (req, res) => {
+//   // res.writeHead(200, { 'Content-Type': 'text/html' });
+//   // res.write('<!DOCTYPE html><html><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><link type="text/css" href="/style.css" as="style"><script type="text/javascript;charset=utf-8" src="/paper-full.js"></script><script type="text/javascript;charset=utf-8" src="/genuary-1-2022.js" canvas="myCanvas"></script></head><body><h1>header</h1><canvas id="myCanvas"></canvas></body></html>');
+//   // res.sendFile(__dirname + "../views/genuary-1-2022.html");
+//   // res.sendFile(path.join(__dirname, "../views/genuary-1-2022.html"));
+//   // res.sendFile(path.join(__dirname, "../views/genuary-1-2022.html");
+//   // res.end();
+
+
+//   res.writeHead(200, { 'Content-Type': 'text/html' });
+//   res.sendFile(path.join(__dirname, '../views/genuary-1-2022.html'));
+// });
+
 // router.get('/', (req, res) => res.sendFile(path.join(__dirname, "../views/genuary-1-2022.html")));
-router.get('/static/stylesheets/style.css', (req, res) => res.sendFile(path.join(__dirname, '../public/stylesheets/style.css')));
-router.get('/static/javascripts/genuary-1-2022.js', (req, res) => res.sendFile(path.join(__dirname, '../public/javascripts/genuary-1-2022.js')));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+// router.get('/style.css', (req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'text/css' });
+//   res.sendFile(path.join(__dirname, '../public/stylesheets/style.css'));
+// });
+// router.get('/genuary-1-2022.js', (req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'text/javascript;charset=utf-8' });
+//   res.sendFile(path.join(__dirname, '../public/javascripts/genuary-1-2022.js'));
+// });
+// router.get('/paper-full.js', (req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'text/javascript' });
+//   res.sendFile(path.join(__dirname, '../node_modules/paper/dist/paper-full.js'));
+// });
+
+// router.get('/paper-full.js', (req, res) => res.sendFile(path.join(__dirname, '../node_modules/paper/dist/paper-full.js')));
+router.get('/', (req, res) => res.sendFile(path.join(__dirname, '../views/genuary-1-2022.html')));
+
+// router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 // router.get('/', (req, res) => {
 //   res.sendFile(__dirname + "../views/genuary-1-2022.html");
